0f73af95fc8d177a92b3ded74f57eff89ef53166
Osman El-Ghotmi <oelgh031@uottawa.ca>
2023-03-30 14:49:50 -0400

    starting rotate array

---
+++ rotate_array.py
@@ -0,0 +1,14 @@
+class Solution:
+  def rotate(self, nums, k):
+
+
+def example_1():
+  nums = [1, 2, 3, 4, 5, 6, 7]
+  expected_result = [5, 6, 7, 1, 2, 3, 4]
+  k = 3
+
+def example_2():
+  nums = [-1, -100, 3, 99]
+  k = 2
+  expected_result = [3, 99, -1, -100]
+
