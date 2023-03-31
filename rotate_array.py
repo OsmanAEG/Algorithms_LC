13174f5f89575889f1088ebe83e069c1b16e75de
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-03-31 11:15:30 -0400

    rotate array

--- rotate_array.py
+++ rotate_array.py
@@ -1,4 +1,5 @@
-class Solution:
+# naive implementation
+class Solution_1:
   def shift_one_time(unshifted_list):
     tmpi = unshifted_list[0]
     tmpn = unshifted_list[-1]
@@ -16,6 +17,21 @@ class Solution:
 
     return nums
 
+class Solution_2:
+  def rotate(self, nums, k):
+    shifted_nums = []
+    n = len(nums)
+
+    # add the end of the list
+    for i in range(n-k, n):
+      shifted_nums.append(nums[i])
+
+    # add the begining of the list
+    for i in range(n-k):
+      shifted_nums.append(nums[i])
+
+    return shifted_nums
+
 def check_result(result, expected_result):
   n = len(result) - 1
   for i in range(n):
@@ -25,7 +41,7 @@ def example_1():
   nums = [1, 2, 3, 4, 5, 6, 7]
   k = 3
   expected_result = [5, 6, 7, 1, 2, 3, 4]
-  solution_handler = Solution
+  solution_handler = Solution_2
   result = solution_handler.rotate(solution_handler, nums, k)
   check_result(result, expected_result)
   print("The result for example 1 is: " + str(result))
@@ -34,10 +50,10 @@ def example_2():
   nums = [-1, -100, 3, 99]
   k = 2
   expected_result = [3, 99, -1, -100]
-  solution_handler = Solution
+  solution_handler = Solution_2
   result = solution_handler.rotate(solution_handler, nums, k)
   check_result(result, expected_result)
-  print("The result for example 1 is: " + str(result))
+  print("The result for example 2 is: " + str(result))
 
 example_1()
 example_2()
